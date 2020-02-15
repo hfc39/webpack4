636ca995756b439552cc57ca313dbd8649e2381e
Carmen Chen <carmen39@gmail.com>
2020-02-15 15:54:56 +0800

    install server node and packages installed: express, path, body-parser, cors, aylien_textapi

---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+projectData = {};
+var path = require('path');
+const express = require('express')
+//const mockAPIResponse = require('./mockAPI.js')
+const app = express();
+
+/*Dependency body parser*/
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+/*cors*/
+const cors = require('cors');
+app.use(cors());
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+// designates what port the app will listen to for incoming requests
+const port = 8000;
+app.listen(port, function () {
+    console.log('Example app listening on port'${port});
+})
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
