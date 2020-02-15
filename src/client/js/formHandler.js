e56c1bfcf0eaa11819650eacedcfa9c36b56b538
Carmen Chen <carmen39@gmail.com>
2020-02-16 00:30:08 +0800

    fix formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,9 +1,25 @@
 function handleSubmit(event) {
     event.preventDefault()
-
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('input').value
+    const sendDataAylien = async function (url='', data={}) {
+        const response = await fetch ( '/aylien', {
+            method:'POST',
+            credentials:'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        try{
+            const newData = response.json();
+            document.getElementById('newURL').innerText = `Submitted URL:${newData.URL}`;
+            document.getElementById('newPo').innerText = newData.;
+            document.getElementById('newScore').innerText =;
+        } catch (error){
+            console.log('failed at sendDataAylien');
+        }
+    }
 
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8080/test')
