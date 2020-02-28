5fb23a59ab9aacc601c1086c7d6bda4d23d057a6
Carmen Chen <carmen39@gmail.com>
2020-02-28 17:36:36 +0800

    install dev: clean-wepback-plugin and webpack server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
-    devtool: 'source-map',
+    //devtool: 'source-map',
     stats: 'verbose',
     module: {
         rules: [
@@ -26,14 +26,6 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
+        new CleanWebpackPlugin()
     ]
 }
